0ef031050c367f68dd3d8c8a88edfa6025839c58
laurenkafkaloff <laurenkaf@gmail.com>
2021-04-23 15:12:09 -0400

    quer-ayyyyy

--- backend/Routes/laurenRouter.js
+++ backend/Routes/laurenRouter.js
@@ -47,6 +47,14 @@ const topWordsByGenre = (req, res) => {
 // Top {limit} used lyrics in top {num} songs in the time range {lower} to {upper}
 // most popular words in top 10 songs
 // allow ppl to set range and rank
+
+// SELECT l.word, COUNT(l.word) as count
+// FROM Lyric l JOIN HasLyric h ON l.word = h.word
+// JOIN BillboardAppearance b ON b.song_id = h.song_id
+// WHERE b.position <= 10 AND YEAR(b.week) >= 2000 AND YEAR(b.week) <= 2010
+// GROUP BY l.word
+// ORDER BY COUNT(l.word) DESC
+// LIMIT 20;
 const topWordsByRankAndTime = (req, res) => {
   const limit = 10;
   const num = 10;
@@ -74,6 +82,17 @@ const topWordsByRankAndTime = (req, res) => {
 // What portion of Top 100 songs dominated by a specific genre?
 // Percentage of songs from each genre (top {limit}) in time range {lower} to {upper}
 // which genres are most commonly on the ranks
+
+// WITH timeRangeCompiled AS (
+//   SELECT g.category, COUNT(g.category) as num
+//   FROM BillboardAppearance b JOIN Genre g ON b.song_id = g.song_id
+//   WHERE YEAR(b.week) >= 2010 AND YEAR(b.week) <= 2020
+//   GROUP BY g.category
+// )
+// SELECT category, num/(SELECT SUM(num) FROM timeRangeCompiled) as percentage
+// FROM timeRangeCompiled 
+// ORDER BY num DESC
+// LIMIT 20;
 const topWordsByRankAndTime = (req, res) => {
   const limit = 10;
   const lower = 2000;
@@ -103,6 +122,24 @@ const topWordsByRankAndTime = (req, res) => {
 
 // Highest chart position of each genre in a certain time range?
 // For each genre that appeared on the chart in the time range {lower} to {upper}, highest position of each genre
+
+// WITH timeRangeCompiled AS (
+//   SELECT g.category, COUNT(g.category) as num
+//   FROM BillboardAppearance b JOIN Genre g ON b.song_id = g.song_id
+//   GROUP BY g.category
+// ),
+// commonGenres AS (
+// SELECT category
+// FROM timeRangeCompiled 
+// ORDER BY num DESC
+// LIMIT 50
+// )
+// SELECT g.category, MIN(b.position) AS highest_position
+// FROM Song s JOIN Genre g ON s.id = g.song_id
+// 		        JOIN BillboardAppearance b ON s.id = b.song_id
+// WHERE YEAR(b.week) >= 2010 AND YEAR(b.week) <= 2020 AND g.category IN (SELECT * FROM commonGenres)
+// GROUP BY g.category
+// ORDER BY highest_position;
 const topPosOfGenre = (req, res) => {
   const limit = 100;
   const lower = 2000;
@@ -139,57 +176,68 @@ const topPosOfGenre = (req, res) => {
 };
 
 
+// Search for everything
 
+// SELECT DISTINCT p.title, p.performer
+// FROM Song s, Genre g, BillboardAppearance b, HasLyric l, PerformerTitle p
+// WHERE 
+// ('pop' IS NULL OR (s.id = g.song_id AND g.category = 'pop')) AND
+// (1950 IS NULL OR (s.id = b.song_id AND YEAR(b.week) >= 1950)) AND 
+// (2020 IS NULL OR (s.id = b.song_id AND YEAR(b.week) <= 2020)) AND 
+// (10 IS NULL OR (s.id = b.song_id AND b.position <= 10)) AND 
+// ("britney spears" IS NULL OR (s.id = p.song_id AND p.performer LIKE "britney spears%")) AND 
+// ("gimme more" IS NULL OR (s.id = p.song_id AND p.title LIKE "gimme more%")) AND 
+// (NULL IS NULL OR (s.album LIKE "lover%")) AND
+// ("i" IS NULL OR (s.id = l.song_id AND l.word = "i")) 
+// LIMIT 10;
 
+// SELECT DISTINCT s.id, s.album
+// FROM Song s, Genre g, BillboardAppearance b, HasLyric l, PerformerTitle p
+// WHERE 
+// (NULL IS NULL OR (s.id = g.song_id AND g.category = 'pop')) AND
+// (NULL IS NULL OR (s.id = g.song_id AND g.category = 'pop')) AND
+// (NULL IS NULL OR (s.id = b.song_id AND YEAR(b.week) >= 1950)) AND 
+// (NULL IS NULL OR (s.id = b.song_id AND YEAR(b.week) <= 2020)) AND 
+// (NULL IS NULL OR (s.id = b.song_id AND b.position <= 10)) AND 
+// ("britney spears" IS NULL OR (s.id = p.song_id AND p.performer LIKE "britney spears%")) AND 
+// (NULL IS NULL OR (s.id = p.song_id AND p.title LIKE "you need to calm down%")) AND 
+// (NULL IS NULL OR (s.album LIKE "lover%")) AND
+// ("i" IS NULL OR (s.id = l.song_id AND l.word = "i")) 
+// LIMIT 10;
+const searchEverything = (req, res) => {
+  const limit = 100;
+  const genre = 'pop';
+  const lower = 2000;
+  const upper = 2010;
+  const pos = 10;
+  const performer = 'britney spears';
+  const title = 'gimme more'; 
+  const album = 'blackout'; 
+  const lyric = 'i'; // req.params.lyric;
 
+  var query = `
+    SELECT DISTINCT p.title, p.performer
+    FROM Song s, Genre g, BillboardAppearance b, HasLyric l, PerformerTitle p
+    WHERE 
+    ('${genre}' IS NULL OR (s.id = g.song_id AND g.category = '${genre}')) AND
+    (${lower} IS NULL OR (s.id = b.song_id AND YEAR(b.week) >= ${lower})) AND 
+    (${upper} IS NULL OR (s.id = b.song_id AND YEAR(b.week) <= ${upper})) AND 
+    (${pos} IS NULL OR (s.id = b.song_id AND b.position <= ${pos})) AND 
+    ('${performer}' IS NULL OR (s.id = p.song_id AND p.performer LIKE '${performer}' + '%')) AND 
+    ('${title}' IS NULL OR (s.id = p.song_id AND p.title LIKE '${title}' + '%')) AND 
+    ('${album}' IS NULL OR (s.album LIKE '${album}' + '%')) AND
+    ('${lyric}' IS NULL OR (s.id = l.song_id AND l.word LIKE '${lyric}' + '%')) 
+    LIMIT ${limit}
+  `;
 
+  connection.query(query, (err, rows, fields) => {
+    if  (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
 
-
-
-
-
-// SELECT s.id
-// FROM Song s JOIN BillboardAppearance b ON b.song_id = s.id
-// WHERE b.position <= 25 AND YEAR(b.week) >= 2010 AND YEAR(b.week) <= 2020
-// LIMIT 20;
-
-// SELECT l.word, COUNT(l.word) as count
-// FROM Lyric l JOIN HasLyric h ON l.word = h.word
-// JOIN BillboardAppearance b ON b.song_id = h.song_id
-// WHERE b.position <= 10 AND YEAR(b.week) >= 2000 AND YEAR(b.week) <= 2010
-// GROUP BY l.word
-// ORDER BY COUNT(l.word) DESC
-// LIMIT 20;
-
-// WITH timeRangeCompiled AS (
-//   SELECT g.category, COUNT(g.category) as num
-//   FROM BillboardAppearance b JOIN Genre g ON b.song_id = g.song_id
-//   WHERE YEAR(b.week) >= 2010 AND YEAR(b.week) <= 2020
-//   GROUP BY g.category
-// )
-// SELECT category, num/(SELECT SUM(num) FROM timeRangeCompiled) as percentage
-// FROM timeRangeCompiled 
-// ORDER BY num DESC
-// LIMIT 20;
-
-// WITH timeRangeCompiled AS (
-//   SELECT g.category, COUNT(g.category) as num
-//   FROM BillboardAppearance b JOIN Genre g ON b.song_id = g.song_id
-//   GROUP BY g.category
-// ),
-// commonGenres AS (
-// SELECT category
-// FROM timeRangeCompiled 
-// ORDER BY num DESC
-// LIMIT 50
-// )
-
-// SELECT g.category, MIN(b.position) AS highest_position
-// FROM Song s JOIN Genre g ON s.id = g.song_id
-// 		        JOIN BillboardAppearance b ON s.id = b.song_id
-// WHERE YEAR(b.week) >= 2010 AND YEAR(b.week) <= 2020 AND g.category IN (SELECT * FROM commonGenres)
-// GROUP BY g.category
-// ORDER BY highest_position;
 
 
 
